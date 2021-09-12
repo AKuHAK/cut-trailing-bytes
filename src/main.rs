e9573790555b6bcc116c2b43cfa68e8f286c516c
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2021-09-13 00:40:24 +0300

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io;
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::path::PathBuf;
 use structopt::StructOpt;
 use std::num::ParseIntError;
+extern crate humansize;
+use humansize::{FileSize, file_size_opts as options};
 
 use indicatif::{ProgressBar, ProgressStyle};
 
@@ -34,19 +37,26 @@ fn main() -> io::Result<()> {
     let opt = Opt::from_args();
     let filename = &opt.file;
     let mut f = File::open(filename)?;
-    let mut valid_len = 0;
-    let mut tmp_len = 0;
     let mut buffer = [0; 4096];
+    let mut valid_len = f.seek(SeekFrom::End(0)).unwrap();
+    let total = valid_len;
+    let mut tmp_len = 0;
+    let mut n;
 
-    let x = File::metadata(&f)?.len();
-    let pb = ProgressBar::new(x);
+    let pb = ProgressBar::new(valid_len);
     pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})"));
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:.cyan/blue}] {bytes}/{total_bytes} {msg} ({eta})"));
 
     loop {
-        let mut n = f.read(&mut buffer[..])?;
-        pb.inc(4096);
+        if valid_len >= 4096 {
+            valid_len = f.seek(SeekFrom::Current(-4096))?;
+        } else {
+            valid_len = f.seek(SeekFrom::Start(0))?;
+        }
+        n = f.read(&mut buffer[..])?;
+        f.seek(SeekFrom::Current(0))?;
         if n == 0 { break; }
+
         for byte in buffer.bytes() {
             match byte.unwrap() {
                 byte if byte == opt.byte_in_hex => { tmp_len += 1; }
@@ -59,12 +69,20 @@ fn main() -> io::Result<()> {
             n -= 1;
             if n == 0 { break; }
         }
+        pb.inc(4096);
+        // exit if 1st buffer passed
+        tmp_len = 0;
+        if valid_len < 4096 { break; }
+        // exit if at least one char doesnt match
+        if valid_len > f.seek(SeekFrom::Current(-4096))? { break; }
     }
+    pb.finish_at_current_pos();
+    // pb.finish_with_message("done");
     if !opt.dry_run {
         let f = OpenOptions::new().write(true).open(filename);
         f.unwrap().set_len(valid_len)?;
     }
-    println!("cut {} from {} to {}", filename.display(), valid_len + tmp_len, valid_len);
+    println!("cut {} from {} to {}", filename.display(), total.file_size(options::BINARY).unwrap(), valid_len.file_size(options::BINARY).unwrap());
 
     Ok(())
 }
